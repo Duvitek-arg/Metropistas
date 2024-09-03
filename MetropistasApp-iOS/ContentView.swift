d0de1982774fc6507612d4d673f3d2e84df3df1d
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-09-03 18:51:55 -0300

    Initial commit from Xcode

--- MetropistasApp-iOS/ContentView.swift
+++ MetropistasApp-iOS/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     @Query private var items: [Item]
 
     var body: some View {
+        Text("Metropistas")
         NavigationSplitView {
             List {
                 ForEach(items) { item in
